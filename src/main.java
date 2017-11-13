8ebd8c7cf0b52a15b98f2a995086da0e2cad0d9b
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-11-12 20:29:29 -0800

    main clss

---
+++ src/main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/**
+ * Created by Arthur on 11/12/17.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("FrameDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+//4. Size the frame.
+        frame.pack();
+
+//5. Show it.
+        frame.setVisible(true);
+    }
+}
