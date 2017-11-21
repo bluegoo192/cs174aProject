ecfe25de9650c425a4c01289dad85a404ee7500a
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-11-21 09:40:56 -0800

    gitignore

--- src/DbCient.java
+++ src/DbCient.java
@@ -19,38 +19,16 @@ public class DbCient {
     }
 
     private DbCient() {
-        Statement stmt = null;
-        try{
-            //STEP 2: Register JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
+    }
 
-            //STEP 3: Open a connection
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            System.out.println("Connected");
-
-            stmt.close();
-            conn.close();
-        }catch(SQLException se){
-            //Handle errors for JDBC
-            se.printStackTrace();
-        }catch(Exception e){
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        }finally{
-            //finally block used to close resources
-            try{
-                if(stmt!=null)
-                    stmt.close();
-            }catch(SQLException se2){
-            }// nothing we can do
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
+    private boolean connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(HOST, USER, PWD);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public void test() {
