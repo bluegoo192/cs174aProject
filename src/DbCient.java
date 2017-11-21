925daa84914d48604d4b7a3d82be4fdc9fcc31a3
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-11-21 09:46:53 -0800

    updated database connection code

--- src/DbCient.java
+++ src/DbCient.java
@@ -7,7 +7,7 @@ public class DbCient {
     Connection conn;
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://cs174a.engr.ucsb.edu";
+    static final String DB_URL = "jdbc:mysql://cs174a.engr.ucsb.edu:3306/silversteinDB";
 
     //  Database credentials
     static final String USER = "silverstein";
@@ -21,10 +21,12 @@ public class DbCient {
     private DbCient() {
     }
 
-    private boolean connect() {
+    public boolean connect() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(HOST, USER, PWD);
+            Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connected to database");
+            connection.close();
             return true;
         } catch (Exception e) {
             return false;
