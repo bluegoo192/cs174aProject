8bdd00e04818a05ee954144abd5f1094c4085bf8
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-12-02 20:09:28 -0800

    insert stock accounts

--- data/index.js
+++ data/index.js
@@ -3,6 +3,7 @@ const parse = require('csv-parse')
 const mysql = require('mysql');
 const moment = require('moment');
 const files = ["actors", "admins", "customers", "marketAccounts", "stocks"];
+const taxIdMap = {};
 
 var con = mysql.createConnection({
   host: "cs174a.engr.ucsb.edu",
@@ -29,19 +30,36 @@ con.connect();
 // })
 
 // Customers
-// fs.readFile("./customers.csv", function (err, fileData) {
-//   parse(fileData, {columns: true, trim: true}, function(err, rows) {
-//     //console.log(rows);
-//     rows.forEach(function (customer) {
-//       let query = "INSERT INTO Customers VALUES ('"+customer.username+"', '"+customer.STATE+"', '"+
-//         customer.email+"', "+customer.TAXID+", '"+
-//         customer.Phone.replace("(","").replace(")","")+"', '"+
-//         customer.password+"', '"+customer.Name+"')";
-//         console.log(query);
-//       con.query(query, function (error, results, fields) {
-//         if (error) throw error;
-//         console.log("inserted "+customer.Name);
-//       });
-//     });
-//   })
-// })
+fs.readFile("./customers.csv", function (err, fileData) {
+  parse(fileData, {columns: true, trim: true}, function(err, rows) {
+    //console.log(rows);
+    rows.forEach(function (customer) {
+      taxIdMap[customer.TAXID] = customer.username;
+      let query = "INSERT INTO Customers VALUES ('"+customer.username+"', '"+customer.STATE+"', '"+
+        customer.email+"', "+customer.TAXID+", '"+
+        customer.Phone.replace("(","").replace(")","")+"', '"+
+        customer.password+"', '"+customer.Name+"')";
+      con.query(query, function (error, results, fields) {
+        // if (error) throw error;
+        console.log("inserted "+customer.Name);
+      });
+    });
+  })
+})
+
+// Stocks
+fs.readFile("./stocks.csv", function (err, fileData) {
+  console.log(taxIdMap);
+  parse(fileData, {columns: true, trim: true}, function(err, rows) {
+    rows.forEach(function (stock) {
+      let query = "INSERT INTO stock_account VALUES ('"+taxIdMap[stock.TAXID]+":"+stock.ACTORID+"', "+
+        stock.numShares+", '"+taxIdMap[stock.TAXID]+"', '"+
+        stock.ACTORID+"')";
+        console.log(query);
+      con.query(query, function (error, results, fields) {
+        if (error) throw error;
+        console.log("inserted "+stock.TAXID);
+      });
+    });
+  })
+})
