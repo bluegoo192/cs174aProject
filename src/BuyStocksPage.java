1058c3ef3defd50a74b2b7559f0ae3062b94be23
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-12-04 20:45:27 -0800

    fix query

--- src/BuyStocksPage.java
+++ src/BuyStocksPage.java
@@ -183,7 +183,7 @@ public class BuyStocksPage {
 							System.out.println("SUBMITTING TO BUY_STOCK");
 							// Update Buy_Stock and stock_account
 							PreparedStatement statement = DbClient.getInstance().getMainConnection().prepareStatement(
-									"x");
+									"INSERT INTO Buy_Stock VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 							statement.setString(1, Integer.toString(StarsRUs.global_buy+1));
 							statement.setInt(2, quantity); 
 							statement.setString(3, symbol);
